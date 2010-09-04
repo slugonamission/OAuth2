6bac3f34311c00210dc1f89e23ac6780c1dda7b9
Jamie Garside <slugonamission@gmail.com>
2010-09-04 17:22:48 +0100

    Cleaned up example

--- test/main.c
+++ test/main.c
@@ -7,17 +7,26 @@ int main(int argc, char** argv)
     oauth2_set_redirect_uri(conf, "http://notarealhosthonest.com/");
     char* redir_uri = oauth2_request_auth_code(conf, "https://graph.facebook.com/oauth/authorize", "publish_stream", "LOL");
 
-    printf("%s\n", redir_uri);
+    printf("Visit this url and hit authorize: %s\n", redir_uri);
+    printf("Now put the auth token here: ");
+    
+    char code[255];
+    scanf("%s", code);
 
     //Now test token based auth
-    char* code = "received_token";
     char* rv = oauth2_access_auth_code(conf, "https://graph.facebook.com/oauth/access_token", code, NULL);
 
     oauth2_set_auth_code(conf, rv);
-    printf("%s\n", rv);
+    printf("Access Token: %s\n", rv);
     free(rv);
+
+    printf("Enter your Facebook status: ");
+    char status[255];
+    scanf("%s", status);
+    char status2[255];
+    sprintf(status2, "message=%s", status);
     
-    rv = oauth2_request(conf, "https://graph.facebook.com/slugonamission/feed", "message=Hello From oauth2!");
+    rv = oauth2_request(conf, "https://graph.facebook.com/slugonamission/feed", status2);
 
     printf("%s\n", rv);
 
